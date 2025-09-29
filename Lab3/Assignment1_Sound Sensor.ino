3d9ea838adacd5821d82b5912d0e7396e1528ad3
kjsik <127501452+kjsik@users.noreply.github.com>
2025-09-29 18:32:28 +0900

    Update Assignment1_Sound Sensor.ino

--- Lab3/Assignment1_Sound Sensor.ino
+++ Lab3/Assignment1_Sound Sensor.ino
@@ -1,6 +1,6 @@
 #define MIC 36
 #define _LED_R_GPIO_12 12
-#defien _LED_B_GPIO_14 14
+#define _LED_B_GPIO_14 14
 
 int led_state = 0;
 int MIC_threshold = 1500;
@@ -20,12 +20,14 @@ void loop(){
       led_state = 1;
       digitalWrite(_LED_R_GPIO_12, HIGH);
       digitalWrite(_LED_B_GPIO_14, LOW);
+      delay(100);
     }
     else{
       led_state = 0;
       digitalWrite(_LED_R_GPIO_12, LOW);
       digitalWrite(_LED_B_GPIO_14, HIGH);
+      delay(100);
     }
-  delay(200);
+  delay(2);
   }
 }
